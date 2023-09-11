0134628a5d09d2b36492865be39dac98171596e3
grvn-ht <grvn.huet@gmail.com>
2023-09-11 14:27:07 +0200

    ajout parser python pour mqtt

---
+++ mos/mos_pub.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as mqtt 
+import time
+
+broker_hostname = "151.80.152.245"
+port = 1883 
+
+def on_connect(client, userdata, flags, return_code):
+    if return_code == 0:
+        print("connected")
+    else:
+        print("could not connect, return code:", return_code)
+
+client = mqtt.Client("Client1")
+# client.username_pw_set(username="user_name", password="password") # uncomment if you use password auth
+client.on_connect=on_connect
+
+client.connect(broker_hostname, port)
+client.loop_start()
+
+topics = ["temp", "hum","ph","ec"]
+closets = [1,3]
+i=10
+try:
+    while i<=26:
+        for clo in closets:
+            for top in topics:
+                mess = str(clo)+'|'+str(i)
+                time.sleep(2)
+                result = client.publish(top, mess)
+                status = result[0]
+                if status == 0:
+                    print("Message "+ str(mess) + " is published to topic " + top)
+                else:
+                    print("Failed to send message to topic " + top)
+                i += 1
+        time.sleep(4)
+finally:
+    client.loop_stop()
--- mos/mos_sub.py
+++ mos/mos_sub.py
@@ -1,27 +1,36 @@
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 import paho.mqtt.client as mqtt
-import sqlite3
+import time
+import threading
+
+infos_to_insert={}
+# with open('mos_sub/sample_image.jpg', 'rb') as image_file:
+#     image_data = image_file.read()
+# info_to_insert['image']=image_data
 
 # MQTT broker settings
-broker_address =  "151.80.152.245"
+broker_address =  "mosquitto"
 port = 1883
-topics = ["topic1", "topic2"]
+topics = ["temp", "hum","ph","ec"]
 
-# Variables to store the values
-value_topic1 = None
-value_topic2 = None
+def decode_message(message):
+    print('message: '+ message)
+    values = message.split("|")
+    id_closet = values[0]
+    final_value = float(values[1])
+    return (id_closet,final_value)
 
 # Callback when a message is received
 def on_message(client, userdata, message):
-    global value_topic1, value_topic2
+    global infos_to_insert
     
-    if message.topic == "topic1":
-        value_topic1 = float(message.payload.decode())
-    elif message.topic == "topic2":
-        value_topic2 = float(message.payload.decode())
-
-    # Check if both values are available
-    if value_topic1 is not None and value_topic2 is not None:
-        execute_sql_query(value_topic1, value_topic2)
+    id_closet, value = decode_message(message.payload.decode("utf-8"))
+    if id_closet not in infos_to_insert:
+        infos_to_insert[id_closet] = {}  # Create an inner dictionary if it doesn't exist
+    infos_to_insert[id_closet][message.topic]=value
+    print(infos_to_insert[id_closet])
 
 # Create an MQTT client
 client = mqtt.Client()
@@ -36,18 +45,93 @@ client.connect(broker_address, port)
 for topic in topics:
     client.subscribe(topic)
 
-# Start the MQTT client loop
-client.loop_forever()
+# MQTT loop function (asynchronously)
+def mqtt_loop():
+    client.loop_forever()
 
-def execute_sql_query(value_topic1, value_topic2):
-    # Connect to the SQLite database
-    conn = sqlite3.connect('mqtt_data.db')
-    cursor = conn.cursor()
+def insert_infos_to_db(info_to_insert):
+    # Database connection parameters
+    db_url = "postgresql://gur:lemotdepassesecret!!-678@151.80.152.245:5432/fullcloset"
+    engine = create_engine(db_url)
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    # Insert both values into the table
-    cursor.execute("INSERT INTO mqtt_data (topic1_value, topic2_value) VALUES (?, ?)",
-                    (value_topic1, value_topic2))
+    start_date = datetime(2023, 9, 11)
+    info_to_insert['event_date']=start_date
+    Info = Table('info', metadata, autoload=True, autoload_with=engine)
 
-    # Commit the transaction and close the connection
-    conn.commit()
-    conn.close()
+    insert_statement = Info.insert().values(**info_to_insert)
+    session.execute(insert_statement)
+    session.commit()
+
+    session.close()
+
+# Your custom action to run every 5 minutes
+def publish_infos_to_db():
+    global infos_to_insert
+    while True:
+        # Access the global received_messages dictionary to retrieve MQTT messages
+        for id_closet, to_insert in infos_to_insert.items():
+            to_insert['closet_id']=id_closet
+            insert_infos_to_db(to_insert)
+        #reinitialisation du dict pour les 5 prochaines minutes
+        infos_to_insert={} 
+        print('other bagg')
+        time.sleep(60)  # Sleep for 5 minutes (300 seconds)
+
+# Start the MQTT loop in a separate thread
+mqtt_thread = threading.Thread(target=mqtt_loop)
+mqtt_thread.start()
+
+# Start the custom action timer in a separate thread
+custom_action_thread = threading.Thread(target=publish_infos_to_db)
+custom_action_thread.start()
+
+
+# table_names = metadata.tables.keys()
+# for table_name in table_names:
+#     print(f"Table Name: {table_name}")
+
+# select_query = select([User])
+# result = session.execute(select_query)
+# for row in result:
+#     print(row)
+
+# condition = and_(User.c.id.in_([4]))
+# delete_statement = delete(User).where(condition)
+# session.execute(delete_statement)
+# session.commit()
+
+# select_query = select([User])
+# result = session.execute(select_query)
+# for row in result:
+#     print(row)
+
+# Closet = Table('closet', metadata, autoload=True, autoload_with=engine)
+# condition = and_(Closet.c.id.in_([2]))
+# delete_statement = delete(Closet).where(condition)
+# session.execute(delete_statement)
+# session.commit()
+
+# select_query = select([Closet])
+# result = session.execute(select_query)
+# for row in result:
+#     print(row)
+
+# condition = and_(Info.c.id.in_([51]))
+# select_query = select([Info]).where(condition)
+# result = session.execute(select_query)
+# for row in result:
+#     # Open the binary image file
+#     image = Image.open(io.BytesIO(row.image))  # Replace with the path to your binary image file
+
+#     # Display information about the image
+#     print("Image Format:", image.format)
+#     print("Image Mode:", image.mode)
+#     print("Image Size:", image.size)
+
+#     # Show or save the image
+#     image.show()  # Display the image using the default viewer
+#     # image.save('output_image.png')  # Save the image in a different format if needed

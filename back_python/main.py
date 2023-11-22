abd59968c9681488aae35c5e68b713c1730fae1d
grvn-ht <grvn.huet@gmail.com>
2023-11-22 20:47:58 +0100

    post image

--- back_python/main.py
+++ back_python/main.py
@@ -181,18 +181,28 @@ def get_image_data():
     #image_data = Info.query.with_entities(Info.image, Info.created_at).order_by(Info.created_at.desc()).limit(1).all()
     #image_timestamps = [{'image': image, 'timestamp': created_at.isoformat()} for image, created_at in image_data]
     #return jsonify(image_timestamps[0])
-    return send_file('/tmp/sample_image.jpg')
+    return send_file('/tmp/images/sample_image.jpg')
+
+@app.route("/image", methods=["POST"])
+def upload_image():
+    image_path = '/tmp/images/sample_image.jpg'  # Destination path to save the image
+    with open(image_path, 'ab') as f:
+        chunk = request.stream.read(1024)  # Read 1KB at a time
+        while chunk:
+            f.write(chunk)  # Write the chunk to the file
+            chunk = request.stream.read(1024)  # Read the next chunk
+    return 'Image uploaded successfully!'
 
 @app.route("/gif", methods=["GET"])
 #@jwt_required()
 def get_gif_data():
-    #frames = [Image.open('/tmp/sample_image.jpg') for i in range(10)]
-    #frame_one = frames[0]
-    #frame_one.save("/tmp/gif.gif", format="GIF", append_images=frames,
-    #           save_all=True, duration=100, loop=0)
-    #image_data = Info.query.with_entities(Info.image, Info.created_at).order_by(Info.created_at.desc()).limit(1).all()
-    #image_timestamps = [{'image': image, 'timestamp': created_at.isoformat()} for image, created_at in image_data]
-    #return jsonify(image_timestamps[0])
+    frames = [Image.open('/tmp/images/sample_image.jpg') for i in range(10)]
+    frame_one = frames[0]
+    frame_one.save("/tmp/gif.gif", format="GIF", append_images=frames,
+               save_all=True, duration=100, loop=0)
+    image_data = Info.query.with_entities(Info.image, Info.created_at).order_by(Info.created_at.desc()).limit(1).all()
+    image_timestamps = [{'image': image, 'timestamp': created_at.isoformat()} for image, created_at in image_data]
+    return jsonify(image_timestamps[0])
     return send_file('/tmp/gif.gif')
 
 if __name__ != '__main__':

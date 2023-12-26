a44bb9b2f42ca2bf05403b6c1ab0449a8b6f2da4
grvn-ht <grvn.huet@gmail.com>
2023-12-26 12:50:19 +0100

    change mqtt

--- mos/mos_sub.py
+++ mos/mos_sub.py
@@ -92,9 +92,9 @@ def publish_infos_to_db():
                 to_insert['closet_id']=id_closet
                 insert_infos_to_db(to_insert)
 
+            print(infos_to_insert)
             infos_to_insert={} 
-            print('other bagg')
-            time.sleep(60)  # Sleep for 5 minutes (300 seconds)
+            time.sleep(20)  # Sleep for 5 minutes (300 seconds)
         except Exception as e:
             print(e)
             infos_to_insert={}

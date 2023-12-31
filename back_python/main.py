c96ebfc529588561924947d2d1bad187279acc92
grvn-ht <grvn.huet@gmail.com>
2023-12-31 19:21:21 +0100

    wip

--- back_python/main.py
+++ back_python/main.py
@@ -272,8 +272,8 @@ if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    create_sample_data(db, User, Closet, Info,app)
+    #create_sample_data(db, User, Closet, Info,app)
 
 if __name__ == "__main__":
-    create_sample_data(db, User, Closet, Info,app)
+    #create_sample_data(db, User, Closet, Info,app)
     app.run(debug=True)
